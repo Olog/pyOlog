e73c843f2223db8393f489e9861666b17d3214d6
Stuart Wilkins <swilkins@bnl.gov>
2014-11-22 21:51:26 -0500

    Added ologgrab script

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(name='pyOlog',
       author_email='shroffk@bnl.gov',
       packages=['pyOlog'],
       requires=['requests (>=2.0.0)', 'urllib3 (>=1.7.1)'],
-      scripts=['scripts/olog']
+      scripts=['scripts/olog', 'scripts/ologgrab']
      )
